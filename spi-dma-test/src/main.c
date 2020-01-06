545a9842e31d6e06116e493e56e878640b673a2d
Stephan Mühlstrasser <stephan.muehlstrasser@web.de>
2020-01-06 16:39:23 +0100

    SPI_master_slave_fullduplex_dma for Longan Nano
    Attempt to port SPI_master_slave_fullduplex_dma from
    GD32VF103_Firmware_Library to Longan Nano.
    Connect SPI0 (master) and SPI1 (slave) pins.
    Not working.

---
+++ spi-dma-test/src/main.c
@@ -0,0 +1,293 @@
+/*!
+    \file    main.c
+    \brief   SPI fullduplex communication use DMA, adapted for Longan Nano
+*/
+
+/*
+    Copyright (c) 2019, GigaDevice Semiconductor Inc.
+
+    Redistribution and use in source and binary forms, with or without modification, 
+are permitted provided that the following conditions are met:
+
+    1. Redistributions of source code must retain the above copyright notice, this 
+       list of conditions and the following disclaimer.
+    2. Redistributions in binary form must reproduce the above copyright notice, 
+       this list of conditions and the following disclaimer in the documentation 
+       and/or other materials provided with the distribution.
+    3. Neither the name of the copyright holder nor the names of its contributors 
+       may be used to endorse or promote products derived from this software without 
+       specific prior written permission.
+
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED 
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. 
+IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, 
+INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT 
+NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR 
+PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, 
+WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY 
+OF SUCH DAMAGE.
+
+  This example is based on the Longan Nano board, it shows SPI0 and SPI1 fullduplex 
+communication use DMA mode.After the communicate is complete, if receive data is equal to
+send data,led2 and led3 turn on, if not led2 and led3 turn off.
+
+  Connect SPI0 SCK  PIN(PA5) to SPI1 SCK  PIN(PB13).
+  Connect SPI0 MISO PIN(PA6) to SPI1 MISO PIN(PB14).
+  Connect SPI0 MOSI PIN(PA7) to SPI1 MOSI PIN(PB15).
+*/
+
+#include "gd32vf103.h"
+#include "gd32vf103_libopt.h"
+#include "gd32v_pjt_include.h"
+#include "systick.h"
+
+#define  ARRAYSIZE         10
+
+uint8_t spi0_send_array[ARRAYSIZE] = {0xA1, 0xA2, 0xA3, 0xA4, 0xA5, 0xA6, 0xA7, 0xA8, 0xA9, 0xAA};
+uint8_t spi1_send_array[ARRAYSIZE] = {0xB1, 0xB2, 0xB3, 0xB4, 0xB5, 0xB6, 0xB7, 0xB8, 0xB9, 0xBA};
+uint8_t spi0_receive_array[ARRAYSIZE] = { 0x00 }; 
+uint8_t spi1_receive_array[ARRAYSIZE] = { 0x00 };
+ErrStatus memory_compare(uint8_t* src, uint8_t* dst, uint8_t length);
+
+void rcu_config(void);
+void gpio_config(void);
+void dma_config(void);
+void spi_config(void);
+void longan_led_init(void);
+
+/*!
+    \brief      main function
+    \param[in]  none
+    \param[out] none
+    \retval     none
+*/
+int main(void)
+{
+    /* peripheral clock enable */
+    rcu_config();
+    /* GPIO configure */
+    gpio_config();
+    /* DMA configure */
+    dma_config();
+    /* SPI configure */
+    spi_config();
+
+    /* initialize the LEDs */
+    longan_led_init();
+
+    /* SPI enable */
+    spi_enable(SPI1);
+    spi_enable(SPI0);
+
+    LEDG(0);
+    LEDB(0);
+    LEDR(0);
+
+    /* DMA channel enable */
+    dma_channel_enable(DMA0, DMA_CH1);
+    dma_channel_enable(DMA0, DMA_CH2);
+    dma_channel_enable(DMA1, DMA_CH0);
+    dma_channel_enable(DMA1, DMA_CH1);
+
+    /* SPI DMA enable */
+    spi_dma_enable(SPI1, SPI_DMA_TRANSMIT);
+    spi_dma_enable(SPI1, SPI_DMA_RECEIVE);
+    spi_dma_enable(SPI0, SPI_DMA_TRANSMIT);
+    spi_dma_enable(SPI0, SPI_DMA_RECEIVE);
+
+    delay_1ms(1000);
+
+    LEDG(1);
+    LEDB(1);
+    LEDR(1);
+
+    /* wait dma transmit complete */
+    while(!dma_flag_get(DMA0, DMA_CH2, DMA_FLAG_FTF));
+    while(!dma_flag_get(DMA1, DMA_CH1, DMA_FLAG_FTF));
+    while(!dma_flag_get(DMA0, DMA_CH1, DMA_FLAG_FTF));
+    while(!dma_flag_get(DMA1, DMA_CH0, DMA_FLAG_FTF));
+
+    /* compare receive data with send data */
+    if(ERROR != memory_compare(spi1_receive_array, spi0_send_array, ARRAYSIZE)){
+        LEDG(0);
+    }else{
+        LEDG(1);
+    }
+    
+    if(ERROR != memory_compare(spi0_receive_array, spi1_send_array, ARRAYSIZE)){
+        LEDB(0);
+    }else{
+        LEDB(1);
+    }
+
+    LEDR(0);
+
+    while(1);
+}
+
+/*!
+    \brief      configure different peripheral clocks
+    \param[in]  none
+    \param[out] none
+    \retval     none
+*/
+void rcu_config(void)
+{
+    rcu_periph_clock_enable(RCU_GPIOA);
+    rcu_periph_clock_enable(RCU_GPIOB);
+    rcu_periph_clock_enable(RCU_GPIOC);
+    rcu_periph_clock_enable(RCU_AF);
+    rcu_periph_clock_enable(RCU_DMA0);
+    rcu_periph_clock_enable(RCU_DMA1);
+    rcu_periph_clock_enable(RCU_SPI0);
+    rcu_periph_clock_enable(RCU_SPI1);
+}
+
+/*!
+    \brief      configure the GPIO peripheral
+    \param[in]  none
+    \param[out] none
+    \retval     none
+*/
+void gpio_config(void)
+{
+    /* original: SPI0 GPIO config:SCK/PA5, MISO/PA6, MOSI/PA7 */
+    /* Longan Nano: SPI0 GPIO config:SCK/PA5, MISO/PA6, MOSI/PA7 */
+    gpio_init(GPIOA, GPIO_MODE_AF_PP, GPIO_OSPEED_50MHZ, GPIO_PIN_5 | GPIO_PIN_7);
+    gpio_init(GPIOA, GPIO_MODE_IN_FLOATING, GPIO_OSPEED_50MHZ, GPIO_PIN_6);
+    
+    /* original: SPI2 GPIO config:SCK/PC10, MISO/PC11, MOSI/PC12 */
+    /* Longan Nano: SPI1 GPIO config:SCK/PB13, MISO/PB14, MOSI/PB15 */
+    // gpio_pin_remap_config(GPIO_SPI1_REMAP, ENABLE);
+    gpio_init(GPIOB, GPIO_MODE_IN_FLOATING, GPIO_OSPEED_50MHZ, GPIO_PIN_13 | GPIO_PIN_15);
+    gpio_init(GPIOB, GPIO_MODE_AF_PP, GPIO_OSPEED_50MHZ, GPIO_PIN_14);
+}
+
+/*!
+    \brief      configure the DMA peripheral
+    \param[in]  none
+    \param[out] none
+    \retval     none
+*/
+void dma_config(void)
+{
+    dma_parameter_struct dma_init_struct;
+    
+    /* SPI0 transmit dma config:DMA0-DMA_CH2 */
+    dma_deinit(DMA0, DMA_CH2);
+    dma_struct_para_init(&dma_init_struct);
+    
+    dma_init_struct.periph_addr  = (uint32_t)&SPI_DATA(SPI0);
+    dma_init_struct.memory_addr  = (uint32_t)spi0_send_array;
+    dma_init_struct.direction    = DMA_MEMORY_TO_PERIPHERAL;
+    dma_init_struct.memory_width = DMA_MEMORY_WIDTH_8BIT;
+    dma_init_struct.periph_width = DMA_PERIPHERAL_WIDTH_8BIT;
+    dma_init_struct.priority     = DMA_PRIORITY_LOW;
+    dma_init_struct.number       = ARRAYSIZE;
+    dma_init_struct.periph_inc   = DMA_PERIPH_INCREASE_DISABLE;
+    dma_init_struct.memory_inc   = DMA_MEMORY_INCREASE_ENABLE;
+    dma_init(DMA0, DMA_CH2, &dma_init_struct);
+    /* configure DMA mode */
+    dma_circulation_disable(DMA0, DMA_CH2);
+    dma_memory_to_memory_disable(DMA0, DMA_CH2);
+
+    /* SPI0 receive dma config:DMA0-DMA_CH1 */
+    dma_deinit(DMA0, DMA_CH1);
+    dma_init_struct.periph_addr  = (uint32_t)&SPI_DATA(SPI0);
+    dma_init_struct.memory_addr  = (uint32_t)spi0_receive_array;
+    dma_init_struct.direction    = DMA_PERIPHERAL_TO_MEMORY;
+    dma_init_struct.priority     = DMA_PRIORITY_HIGH;
+    dma_init(DMA0, DMA_CH1, &dma_init_struct);
+    /* configure DMA mode */
+    dma_circulation_disable(DMA0, DMA_CH1);
+    dma_memory_to_memory_disable(DMA0, DMA_CH1);
+
+    /* SPI1 transmit dma config:DMA1,DMA_CH1 */
+    dma_deinit(DMA1, DMA_CH1);
+    dma_init_struct.periph_addr  = (uint32_t)&SPI_DATA(SPI1);
+    dma_init_struct.memory_addr  = (uint32_t)spi1_send_array;
+    dma_init_struct.direction    = DMA_MEMORY_TO_PERIPHERAL;
+    dma_init_struct.priority     = DMA_PRIORITY_MEDIUM;
+    dma_init(DMA1, DMA_CH1, &dma_init_struct);
+    /* configure DMA mode */
+    dma_circulation_disable(DMA1, DMA_CH1);
+    dma_memory_to_memory_disable(DMA1, DMA_CH1);
+
+    /* SPI1 receive dma config:DMA1,DMA_CH0 */
+    dma_deinit(DMA1, DMA_CH0);
+    dma_init_struct.periph_addr  = (uint32_t)&SPI_DATA(SPI1);
+    dma_init_struct.memory_addr  = (uint32_t)spi1_receive_array;
+    dma_init_struct.direction    = DMA_PERIPHERAL_TO_MEMORY;
+    dma_init_struct.priority     = DMA_PRIORITY_ULTRA_HIGH;
+    dma_init(DMA1, DMA_CH0, &dma_init_struct);
+    /* configure DMA mode */
+    dma_circulation_disable(DMA1, DMA_CH0);
+    dma_memory_to_memory_disable(DMA1, DMA_CH0);
+}
+
+/*!
+    \brief      configure the SPI peripheral
+    \param[in]  none
+    \param[out] none
+    \retval     none
+*/
+void spi_config(void)
+{
+    spi_parameter_struct spi_init_struct;
+    /* deinitilize SPI and the parameters */
+    spi_i2s_deinit(SPI0);
+    spi_i2s_deinit(SPI1);
+    spi_struct_para_init(&spi_init_struct);
+
+    /* SPI0 parameter config */
+    spi_init_struct.trans_mode           = SPI_TRANSMODE_FULLDUPLEX;
+    spi_init_struct.device_mode          = SPI_MASTER;
+    spi_init_struct.frame_size           = SPI_FRAMESIZE_8BIT;
+    spi_init_struct.clock_polarity_phase = SPI_CK_PL_HIGH_PH_2EDGE;
+    spi_init_struct.nss                  = SPI_NSS_SOFT;
+    spi_init_struct.prescale             = SPI_PSC_8;
+    spi_init_struct.endian               = SPI_ENDIAN_MSB;
+    spi_init(SPI0, &spi_init_struct);
+
+    /* SPI1 parameter config */
+    spi_init_struct.device_mode = SPI_SLAVE;
+    spi_init_struct.nss         = SPI_NSS_SOFT;
+    spi_init(SPI1, &spi_init_struct);
+
+}
+
+/*!
+    \brief      memory compare function
+    \param[in]  src: source data pointer
+    \param[in]  dst: destination data pointer
+    \param[in]  length: the compare data length
+    \param[out] none
+    \retval     ErrStatus : ERROR or SUCCESS
+*/
+ErrStatus memory_compare(uint8_t* src, uint8_t* dst, uint8_t length) 
+{
+    while(length--){
+        if(*src++ != *dst++)
+            return ERROR;
+    }
+    return SUCCESS;
+}
+
+void longan_led_init(void)
+{
+    /* enable the led clock */
+    rcu_periph_clock_enable(RCU_GPIOC);
+    rcu_periph_clock_enable(RCU_GPIOA);
+ 
+    /* configure led GPIO port */ 
+    gpio_init(GPIOC, GPIO_MODE_OUT_PP, GPIO_OSPEED_50MHZ, GPIO_PIN_13);
+    gpio_init(GPIOA, GPIO_MODE_OUT_PP, GPIO_OSPEED_50MHZ, GPIO_PIN_1 | GPIO_PIN_2);
+
+    // 1 means off!
+    LEDR(1);
+    LEDG(1);
+    LEDB(1);
+}
+
