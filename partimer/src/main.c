c40ff75b0901db60a2d834dc77ab795a18650f2e
Stephan Mühlstrasser <stephan.muehlstrasser@web.de>
2020-07-06 22:14:14 +0200

    Duty cycle 75%
    Make it possible to see the PWM effect.

--- partimer/src/main.c
+++ partimer/src/main.c
@@ -118,7 +118,8 @@ void timer_config(void)
     timer_ocinitpara.ocnidlestate = TIMER_OCN_IDLE_STATE_LOW;
     timer_channel_output_config(TIMER1, TIMER_CH_3, &timer_ocinitpara);
 
-    timer_channel_output_pulse_value_config(TIMER1, TIMER_CH_3, 2000);
+    // PWM 75% duty cycle
+    timer_channel_output_pulse_value_config(TIMER1, TIMER_CH_3, 3000);
     timer_channel_output_mode_config(TIMER1, TIMER_CH_3, TIMER_OC_MODE_PWM0);
     timer_channel_output_shadow_config(TIMER1, TIMER_CH_3, TIMER_OC_SHADOW_DISABLE);
 
