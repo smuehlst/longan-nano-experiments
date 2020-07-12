29d9af5dff72859d009eaca764ca228792bb423c
Stephan Mühlstrasser <stephan.muehlstrasser@web.de>
2020-07-12 13:29:01 +0200

    Use TIMER0 as third timer

--- partimer/src/main.c
+++ partimer/src/main.c
@@ -67,6 +67,12 @@ void gpio_config(void)
     gpio_init(GPIOC, GPIO_MODE_OUT_PP, GPIO_OSPEED_50MHZ, GPIO_PIN_13);
     /* Green LED */
     gpio_init(GPIOA, GPIO_MODE_OUT_PP, GPIO_OSPEED_50MHZ, GPIO_PIN_1);
+
+    /* PA4 with 1/10 s delay */
+    gpio_init(GPIOA, GPIO_MODE_OUT_PP, GPIO_OSPEED_50MHZ, GPIO_PIN_4);
+
+    /*configure PA8(TIMER0 CH0) as alternate function*/
+    gpio_init(GPIOA, GPIO_MODE_AF_PP, GPIO_OSPEED_50MHZ, GPIO_PIN_8);
 }
 
 /**
@@ -87,11 +93,23 @@ void timer_config(void)
        - The ITR1(TIMER1) is used as input trigger.
        - external clock mode is used, the counter counts on the rising edges of
          the selected trigger.
+    
+    The TIMERxCLK is fixed to 108 MHz, the TIMER1 counter clock is:
+    108MHz/5400= 20KHz.
+
+    The master timer TIMER1 CH0(PA0) is running at TIMER1 frequency:
+    TIMER1 frequency = (TIMER1 counter clock)/ (TIMER1 period + 1) = 5 Hz 
+    and the duty cycle = TIMER1_CH0CV/(TIMER1_CAR + 1) = 50%
+
+    The TIMER2 CH0(PA6) is running:
+    - At (TIMER1 frequency)/ (TIMER2 period + 1) = 2.5 Hz and a duty cycle
+    equal to TIMER2_CH0CV/(TIMER2_CAR + 1) = 50%
       -------------------------------------------------------------------- */
     timer_oc_parameter_struct timer_ocinitpara;
     timer_parameter_struct timer_initpara;
 
     /* rcu_periph_clock_enable(RCU_TIMER0); */
+    rcu_periph_clock_enable(RCU_TIMER0);
     rcu_periph_clock_enable(RCU_TIMER1);
     rcu_periph_clock_enable(RCU_TIMER2);
 
@@ -118,8 +136,8 @@ void timer_config(void)
     timer_ocinitpara.ocnidlestate = TIMER_OCN_IDLE_STATE_LOW;
     timer_channel_output_config(TIMER1, TIMER_CH_3, &timer_ocinitpara);
 
-    // PWM 75% duty cycle
-    timer_channel_output_pulse_value_config(TIMER1, TIMER_CH_3, 3000);
+    // PWM 50% duty cycle
+    timer_channel_output_pulse_value_config(TIMER1, TIMER_CH_3, 2000);
     timer_channel_output_mode_config(TIMER1, TIMER_CH_3, TIMER_OC_MODE_PWM0);
     timer_channel_output_shadow_config(TIMER1, TIMER_CH_3, TIMER_OC_SHADOW_DISABLE);
 
@@ -167,12 +185,48 @@ void timer_config(void)
     /* TIMER2 update event is used as trigger output */
     timer_master_output_trigger_source_select(TIMER2, TIMER_TRI_OUT_SRC_UPDATE);
 
+    /* TIMER0  configuration */
+    timer_deinit(TIMER0);
+    /* initialize TIMER init parameter struct */
+    timer_struct_para_init(&timer_initpara);
+    timer_initpara.prescaler         = 0;
+    timer_initpara.alignedmode       = TIMER_COUNTER_EDGE;
+    timer_initpara.counterdirection  = TIMER_COUNTER_UP;
+    timer_initpara.period            = 1;
+    timer_initpara.clockdivision     = TIMER_CKDIV_DIV1;
+    timer_initpara.repetitioncounter = 0;
+    timer_init(TIMER0, &timer_initpara);
+
+    /* initialize TIMER channel output parameter struct */
+    timer_channel_output_struct_para_init(&timer_ocinitpara);
+    /* CH0 configuration in PWM0 mode */
+    timer_ocinitpara.outputstate  = TIMER_CCX_ENABLE;
+    timer_ocinitpara.outputnstate = TIMER_CCXN_DISABLE;
+    timer_ocinitpara.ocpolarity   = TIMER_OC_POLARITY_HIGH;
+    timer_ocinitpara.ocnpolarity  = TIMER_OCN_POLARITY_HIGH;
+    timer_ocinitpara.ocidlestate  = TIMER_OC_IDLE_STATE_LOW;
+    timer_ocinitpara.ocnidlestate = TIMER_OCN_IDLE_STATE_LOW;
+    timer_channel_output_config(TIMER0, TIMER_CH_0, &timer_ocinitpara);
+
+    timer_channel_output_pulse_value_config(TIMER0, TIMER_CH_0, 1);
+    timer_channel_output_mode_config(TIMER0, TIMER_CH_0, TIMER_OC_MODE_PWM0);
+    timer_channel_output_shadow_config(TIMER0, TIMER_CH_0, TIMER_OC_SHADOW_DISABLE);
+
+    /* auto-reload preload enable */
+    timer_auto_reload_shadow_enable(TIMER0);
+    /* TIMER0 output enable */
+    timer_primary_output_config(TIMER0, ENABLE);
+    /* slave mode selection: TIMER0 */
+    timer_slave_mode_select(TIMER0, TIMER_SLAVE_MODE_EXTERNAL0);
+    timer_input_trigger_source_select(TIMER0, TIMER_SMCFG_TRGSEL_ITI1);
+
     timer_interrupt_enable(TIMER1, TIMER_INT_CH3);
     timer_interrupt_enable(TIMER2, TIMER_INT_CH0);
 
     /* TIMER counter enable */
     timer_enable(TIMER1);
     timer_enable(TIMER2);
+    timer_enable(TIMER0);
 }
 
 /*!
